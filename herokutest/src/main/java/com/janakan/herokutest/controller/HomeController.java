74a289c97140d9b16b23b92bcb074a8d25308460
B030771 <Ramamurthy.Gopalakrishnan@BNSF.com>
2018-09-22 15:58:39 -0500

    Testing config vars

--- herokutest/src/main/java/com/janakan/herokutest/controller/HomeController.java
+++ herokutest/src/main/java/com/janakan/herokutest/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.janakan.herokutest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	@Value("${TEST_KEY}")
+	private String testKey;
+
 	@GetMapping("")
 	public String main(Model model) {
+		model.addAttribute("testKey", testKey);
 		return "home";
 	}
 }
