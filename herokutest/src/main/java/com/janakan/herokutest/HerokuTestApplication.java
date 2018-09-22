54c2d1afeacb4f4e73420275a71aa07c1dff0cdb
B030771 <Ramamurthy.Gopalakrishnan@BNSF.com>
2018-09-22 15:48:10 -0500

    Initial commit

---
+++ herokutest/src/main/java/com/janakan/herokutest/HerokuTestApplication.java
@@ -0,0 +1,11 @@
+package com.janakan.herokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTestApplication.class, args);
+	}
+}
